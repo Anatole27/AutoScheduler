55b175daac3633ab5a87a960fc19d3ebc3d59d68
Anatole Verhaegen <anatole.verhaegen@gmail.com>
2019-02-18 20:16:19 +0100

    MAJ taches

--- src/autoscheduler/schedulers/ScheduleComputer.java
+++ src/autoscheduler/schedulers/ScheduleComputer.java
@@ -18,17 +18,20 @@ public class ScheduleComputer {
 		WorkCalendar calendar;
 		calendar = new WorkCalendar("01/01/2019", 365);
 
-		Day today = calendar.getDay("13/01/2019");
+		Day today = calendar.getDay("18/02/2019");
 
 		/* Tasks */
-		Task[] tasks = new Task[15];
+		Task[] tasks = new Task[17];
 
-		tasks[0] = new Task("01-UFS3", "DAMAV", 5 * WorkCalendar.HOURS_A_DAY);
+		tasks[0] = new Task("01-UFS3", "DAMAV", 4 * WorkCalendar.HOURS_A_DAY);
 		tasks[1] = new Task("02-Comm", "DAMAV", 3 * WorkCalendar.HOURS_A_DAY);
+		tasks[16] = new Task("02-Rapport", "DAMAV", 2 * WorkCalendar.HOURS_A_DAY);
 
 		tasks[0].setStartDay(calendar.getDay("01/01/2019"));
 		tasks[1].addDepTask(tasks[0]);
-		tasks[1].setDeadline(calendar.getDay("31/03/2019"));
+		tasks[1].setDeadline(calendar.getDay("30/04/2019"));
+		tasks[16].addDepTask(tasks[0]);
+		tasks[16].setDeadline(calendar.getDay("31/03/2019"));
 
 		tasks[2] = new Task("01-UFSTD3", "VISIO", 5 * WorkCalendar.HOURS_A_DAY);
 		tasks[3] = new Task("02-Integration", "VISIO", 5 * WorkCalendar.HOURS_A_DAY);
@@ -47,13 +50,13 @@ public class ScheduleComputer {
 		tasks[5].setDeadline(calendar.getDay("31/05/2019"));
 		tasks[6].setDeadline(calendar.getDay("31/05/2019"));
 
-		tasks[7] = new Task("Biblio", "MMACT", 24); // 70 + 86
+		tasks[7] = new Task("Biblio", "MMACT", 0); // 70 + 86
 		tasks[8] = new Task("Def propu", "MMACT", 0);
-		tasks[9] = new Task("WP210", "MMACT", 100); // 180 + 196
-		tasks[10] = new Task("WP220", "MMACT", 20);
-		tasks[11] = new Task("WP310", "MMACT", 120); // 120, 90, 40, 32
-		tasks[12] = new Task("WP320", "MMACT", 90);
-		tasks[13] = new Task("WP330", "MMACT", 40);
+		tasks[9] = new Task("WP210", "MMACT", 30); // 180 + 196
+		tasks[10] = new Task("WP220", "MMACT", 5);
+		tasks[11] = new Task("WP310", "MMACT", 20); // 120, 90, 40, 32
+		tasks[12] = new Task("WP320", "MMACT", 40);
+		tasks[13] = new Task("WP330", "MMACT", 20);
 		tasks[14] = new Task("WP410", "MMACT", 32);
 
 		tasks[7].setStartDay(calendar.getDay("01/01/2019"));
@@ -70,13 +73,18 @@ public class ScheduleComputer {
 		tasks[14].addDepTask(tasks[13]);
 		tasks[14].setDeadline(calendar.getDay("01/06/2019"));
 
+		tasks[15] = new Task("GNSS", "V2020", 60); // 70 + 86
+		tasks[15].setStartDay(calendar.getDay("18/02/2019"));
+		tasks[15].setDeadline(calendar.getDay("15/03/2019"));
+
 		/* Holidays */
-		// Tignes
-		calendar.setHolidays("21/01/2019", "25/01/2019");
 
 		// Frequent holidays
-		calendar.setHolidays("21/04/2019", "27/04/2019");
-		calendar.setHolidays("21/05/2019", "27/05/2019");
+		calendar.setHolidays("01/02/2019"); // Italie
+		calendar.setHolidays("11/03/2019", "15/03/2019"); // Ski rando
+		calendar.setHolidays("19/03/2019"); // Ski tis
+		calendar.setHolidays("25/03/2019", "28/03/2019"); // Fuerteventura
+		calendar.setHolidays("27/05/2019", "02/06/2019"); // Stage MACIF
 		calendar.setHolidays("21/06/2019", "27/06/2019");
 		calendar.setHolidays("21/07/2019", "27/07/2019");
 		calendar.setHolidays("21/08/2019", "27/08/2019");
--- src/autoscheduler/utils/Utils.java
+++ src/autoscheduler/utils/Utils.java
@@ -3,20 +3,17 @@ package autoscheduler.utils;
 import java.util.Iterator;
 import java.util.Vector;
 
-import autoscheduler.types.Task;
-
 public class Utils {
 
 	public static double max(Vector<Double> vec) throws Exception {
 		Iterator<Double> vecIt = vec.iterator();
-		if(vecIt.hasNext()) {
+		if (vecIt.hasNext()) {
 			double maxVal = vecIt.next();
-			while(vecIt.hasNext()) {
-				maxVal = Math.max(maxVal,vecIt.next());
+			while (vecIt.hasNext()) {
+				maxVal = Math.max(maxVal, vecIt.next());
 			}
 			return maxVal;
-		}
-		else {
+		} else {
 			throw new Exception("Empty vector");
 		}
 	}
